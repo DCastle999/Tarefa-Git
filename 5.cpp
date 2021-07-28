7426bc96bd40e60d3e4ebf30bcc367fe18cb7b68
Ricardo Henrique <ricardohennr@gmail.com>
2021-07-28 16:22:20 -0300

    Primeiro Teste

---
+++ 5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main ()
+{
+	setlocale(LC_ALL, "portuguese");
+	int num, i, div=0;
+	cout << "Verificador de Números Primos" << endl;
+	cout << "Digite um número: ";
+	cin >> num;
+	for(i=2; i<=num/2; i++ )
+	{
+		if (num % i == 0)
+			div++;
+	}
+	if (div==0)
+	cout << num << " é um número primo." << endl;
+	else
+	cout << num << " não é um número primo." << endl;	
+	return 0;
+}
